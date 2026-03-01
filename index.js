5836a30abd83ef1dfabe0a941188af75244e06fc
Logan <admin@loganlyons.me>
2026-03-01 02:20:16 -0600

    started implementing cooldowns

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+require('dotenv').config({ path: './config/.env' })
+const token = process.env.token;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+
 client.commands = new Collection();
+client.cooldowns = new Collection();
+
+// gets path of commands folder and reads contents of the directory
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
+// iterate through contents of commands folder
 for (const folder of commandFolders) {
+	// gets full path of files, and filters our anything that doesn't end in '.js' 
 	const commandsPath = path.join(foldersPath, folder);
 	const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+
+	// iterates through the files(our commands)
 	for (const file of commandFiles) {
+		// gets full path of file
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
+		// checks for existence of 'data' and 'execute' in files, else console log error
 		if ('data' in command && 'execute' in command) {
 			client.commands.set(command.data.name, command);
 		} else {
@@ -23,6 +34,7 @@ for (const folder of commandFolders) {
 	}
 }
 
+// everything below this comment is very similar, though it iterates through events
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith('.js'));
 
